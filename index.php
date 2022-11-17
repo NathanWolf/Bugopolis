64b6f17cfe2e91368cf6ab8fdb52678b278d9271
Nathan Wolf <nathan@elmakers.com>
2022-11-17 07:38:04 -0800

    Add some structure to the js folder

--- index.php
+++ index.php
@@ -7,18 +7,18 @@ require_once('php/cache.inc.php');
     <meta charset="UTF-8">
     <title>Bugopolis</title>
     <link rel="stylesheet" href="<?= getCacheUrl('css/common.css'); ?>" type="text/css"/>
-    <script type="text/javascript" src="<?= getCacheUrl('js/main.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/vector.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/timer.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/keyboard.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/viewport.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/stage.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/actor.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/brain.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/player.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/physical_actor.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/living_actor.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/mantis.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/bugopolis/main.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/vector.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/timer.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/keyboard.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/viewport.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/stage/stage.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/actor/actor.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/actor/brain/brain.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/actor/brain/player.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/actor/physical_actor.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/actor/living_actor.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/actor/bug/mantis.js'); ?>"></script>
     <script type="text/javascript">
         document.addEventListener('DOMContentLoaded', onReady, false);
     </script>
