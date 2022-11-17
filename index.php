48d1398af52e5042d4e17afe1ba68be971d315fe
Nathan Wolf <nathan@elmakers.com>
2022-11-17 13:42:57 -0800

    Change file names to match class names

--- index.php
+++ index.php
@@ -8,17 +8,17 @@ require_once('php/cache.inc.php');
     <title>Bugopolis</title>
     <link rel="stylesheet" href="<?= getCacheUrl('css/common.css'); ?>" type="text/css"/>
     <script type="text/javascript" src="<?= getCacheUrl('js/bugopolis/main.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/utility/vector.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/utility/timer.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/utility/keyboard.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/utility/viewport.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/world/world.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/entity/entity.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/entity/brain/brain.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/entity/brain/player.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/entity/physical_entity.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/entity/living_entity.js'); ?>"></script>
-    <script type="text/javascript" src="<?= getCacheUrl('js/entity/bug/mantis.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/Vector.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/Timer.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/Keyboard.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/utility/Viewport.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/world/World.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/entity/Entity.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/entity/brain/Brain.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/entity/brain/Player.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/entity/PhysicalEntity.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/entity/LivingEntity.js'); ?>"></script>
+    <script type="text/javascript" src="<?= getCacheUrl('js/entity/bug/Mantis.js'); ?>"></script>
     <script type="text/javascript">
         document.addEventListener('DOMContentLoaded', onReady, false);
     </script>
